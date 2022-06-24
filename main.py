13e3ddcf62906cd696cb5f7c1cf80b59da97e04f
Ronnie1320 <68396806+Ronnie1320@users.noreply.github.com>
2022-06-24 17:20:58 +0200

    Fix for ModuleNotFoundError

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import os
+import src.csvparser
 
+DT = None
 try:
     if "DISCORD_TOKEN" in os.environ:
         raise ImportError
     import config
+
     DT = config.DISCORD_TOKEN
 except ImportError:
-    config = None
-    DT = os.getenv("DISCORD_TOKEN")
-except ModuleNotFoundError:
-    print("Keine Konfiguration gefunden.")
-    print("Gehen sie zu https://github.com/mainquestministries/mq_bot/wiki/How-to-run#create-a-configpy "
-          "um wiederholen sie die dort genannten Schritte.")
+    try:
+        config = None
+        if "DISCORD_TOKEN" not in os.environ:
+            raise OSError
+        DT = os.getenv("DISCORD_TOKEN")
+    except OSError:
+        print("Keine Konfiguration gefunden.")
+        print("Gehen sie zu https://github.com/mainquestministries/mq_bot/wiki/How-to-run#create-a-configpy "
+              "um wiederholen sie die dort genannten Schritte.")
 
-import src.csvparser
+        exit(1)
 
 if __name__ == "__main__":
     src.main(DT, src.csvparser.CsvParser("Losungen.csv"))
